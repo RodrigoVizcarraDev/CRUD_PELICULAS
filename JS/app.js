7798c39b40d4eadc8ef2746a6e0ed9617fe24ea7
Rodrigo Augusto Vizcarra <rodrigovizcarra9623@gmail.com>
2023-04-20 17:25:53 -0300

    Agregamos al localstorage el tema dark

---
+++ JS/app.js
@@ -0,0 +1,17 @@
+// leer del localstorage el tema o usar el valor por defecto dark
+let temaConfigurado = JSON.parse(localStorage.getItem("theme")) || "dark";
+cambiarTema(temaConfigurado);
+
+let btnThemeDark = document.querySelector("#btnThemeDark");
+let btnThemeLight = document.querySelector("#btnThemeLight");
+
+btnThemeDark.addEventListener("click", () => cambiarTema("dark"));
+btnThemeLight.addEventListener("click", () => cambiarTema("light"));
+
+function cambiarTema(color){
+    let etiquetaHTML = document.querySelector("html");
+    //console.log(etiquetaHTML);
+    etiquetaHTML.setAttribute("data-bs-theme", color);
+    // guardar en localstorage
+    localStorage.setItem("theme", JSON.stringify(color));
+}
